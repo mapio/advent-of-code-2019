8d94f2f2db7b554d5bd18a953abc108a390a67f7
Massimo Santini <massimo.santini@gmail.com>
2019-12-11 01:30:14 +0100

    Refactored

--- day10/sol.py
+++ day10/sol.py
@@ -1,10 +1,7 @@
-#!/usr/bin/env python
-# coding: utf-8
+from math import atan2
+from collections import defaultdict
 
-# In[115]:
-
-
-RAW ="""#...##.####.#.......#.##..##.#.
+RAW = """#...##.####.#.......#.##..##.#.
 #.##.#..#..#...##..##.##.#.....
 #..#####.#......#..#....#.###.#
 ...#.#.#...#..#.....#..#..#.#..
@@ -37,72 +34,39 @@ RAW ="""#...##.####.#.......#.##..##.#.
 #..###.....##.#.......#.##...##
 """
 
-DATA =list(map(list, RAW.splitlines()))
+MAP = list(map(list, RAW.splitlines()))
 
+def dist2(dc, dr):
+  return dc * dc + dr * dr
 
-# In[121]:
-
-
-from math import atan2
-
-def d(rr, cc):
-    CH = DATA[rr][cc].lower()
-    if CH == '.': return dict()
-    los2ch = dict()
-    cmset = set()
-    for r, row in enumerate(DATA):
+# returns a map from {alpha: (dist2, (r, c)) for all (r, c) in direction alpha wrt (rr, cc)}
+def to_los(rr, cc):
+    if MAP[rr][cc] == '.': return dict()
+    line_of_sight = defaultdict(list)
+    for r, row in enumerate(MAP):
         for c, ch in enumerate(row):
-            if r == rr and c == cc: continue
-            if ch != '.':
-                delta = (c-cc),(r-rr)
-                los = atan2(*delta)
-                if los in los2ch:
-                    los2ch[los].append((delta[0] * delta[0] + delta[1] * delta[1], (r, c)))
-                else:
-                    los2ch[los] = [(delta[0] * delta[0] + delta[1] * delta[1], (r, c))]
-    return los2ch
+            if (r == rr and c == cc) or ch == '.': continue
+            delta = (c-cc), (r-rr)
+            line_of_sight[atan2(*delta)].append((dist2(*delta), (r, c)))
+    return line_of_sight
 
+max_los = dict()
+for r in range(len(MAP)):
+    for c in range(len(MAP[0])):
+        los = to_los(r, c)
+        if len(los) > len(max_los):
+            max_los = los
 
-# In[122]:
+print('1)', len(max_los))
 
-
-max_ast = -1
-witness = None
-
-for r in range(len(DATA)):
-    for c in range(len(DATA[0])):
-        #print(d(r, c) if DATA[r][c] != '.' else 0, end = ' ')
-        n = d(r, c)
-        if len(n) > max_ast:
-            max_ast = len(n)
-            witness = r, c
-
-print(witness, max_ast)
-
-
-# In[124]:
-
-
-TOOUT = set([1, 2, 3, 10, 20, 30, 50, 100, 199, 200, 201, 299])
-
-OUT = [[DATA[r][c]*2 for c in range(len(DATA[0]))] for r in range(len(DATA))]
-OUT[3][8] = 'XX'
-res = d(22, 17)
-keys = sorted(res.keys())[::-1]
+alphas = sorted(max_los.keys())[::-1]
 n = 0
-
-while any(len(res[k]) for k in keys):
-    for k in keys:
-        if res[k]:
-            delta, (r, c) = min(res[k])
-            res[k].remove((delta, (r, c)))
+while any(len(max_los[alpha]) for alpha in alphas):
+    for alpha in alphas:
+        if max_los[alpha]:
+            dist, (r, c) = min(max_los[alpha]) # this is the first in line of sight
+            max_los[alpha].remove((dist, (r, c)))
             n += 1
-            OUT[r][c] = n
-            if n in TOOUT: print(n, r, c)
-
-
-# In[ ]:
-
-
-
-
+            if n == 200:
+              print('2)', 100 * c + r)
+              break
